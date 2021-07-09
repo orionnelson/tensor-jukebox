29cd89c1794245e4fb425279e03d299366e4c9b9
orionnelson <orionnelson@hotmail.com>
2021-07-09 03:47:52 -0400

    Fixed Typo

--- speechrec.py
+++ speechrec.py
@@ -8,7 +8,7 @@ processor = Wav2Vec2Processor.from_pretrained('lib\wav2vec2-large-960h-lv60')
 model = Wav2Vec2ForCTC.from_pretrained('lib\wav2vec2-large-960h-lv60')
 
 # load audio
-audio_input, sample_rate = sf.read('/lib/wav/Test.wav')
+audio_input, sample_rate = sf.read('lib\wav\Test.wav')
 
 # pad input values and return pt tensor
 input_values = processor(audio_input, sampling_rate=sample_rate, return_tensors="pt").input_values
